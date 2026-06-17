c3888a4226b1a93f9538efee6c87fd7ba9765247
Evian <zhu.huibin@qq.com>
2026-06-18 00:26:49 +0800

    chore: update Homebrew formula

--- homebrew/termy-server.rb
+++ homebrew/termy-server.rb
@@ -1,13 +1,13 @@
 class TermyServer < Formula
   desc "Terminal PTY backend for the Termy Obsidian plugin (魔改 fork)"
   homepage "https://github.com/pku-zhb/Termy"
-  version "1.4.1"
+  version "1.5.0"
   license "GPL-3.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/pku-zhb/Termy/releases/download/1.4.1/termy-server-darwin-arm64"
-      sha256 "fbe69049dc6013039fa449d8f4445537a80d8c2991eb2a890855fa531708b18a"
+      url "https://github.com/pku-zhb/Termy/releases/download/1.5.0/termy-server-darwin-arm64"
+      sha256 "16fb30e15d69a6032511716070e0ef3cf986d1cd1aaba902ce4bd461bbc9317d"
     end
     # Intel 款 Mac 需要在 release 里补一个 termy-server-darwin-x86_64 资产，再加 on_intel 块。
   end
